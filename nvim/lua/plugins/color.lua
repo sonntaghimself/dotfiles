ae22d5a60a1ef8c7985d11a1005a1b73f20a2319
sonntaghimself <samuel@sonntag.tv>
2024-11-15 15:22:18 +0100

    Some Colorscheme stuff

---
+++ nvim/lua/plugins/color.lua
@@ -0,0 +1,20 @@
+return {
+  -- add colorschemes
+  -- { "ellisonleao/gruvbox.nvim" },
+  -- { "savq/melange-nvim" },
+  { "navarasu/onedark.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  -- Configure LazyVim load colorschem
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "catppuccin-macchiato",
+      colorscheme = "onedark",
+      -- colorscheme = "gruvbox",
+      -- colorscheme = "melange",
+      -- colorscheme = "catppuccin-mocha",
+      -- colorscheme = "catppuccin-frappe",
+    },
+  },
+}
