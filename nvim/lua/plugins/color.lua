3be898e66f36cdf6b03aa3e56c67d6cf3866a0d6
sonntaghimself <samuel@sonntag.tv>
2024-11-15 15:29:08 +0100

    Frappe

--- nvim/lua/plugins/color.lua
+++ nvim/lua/plugins/color.lua
@@ -2,19 +2,19 @@ return {
   -- add colorschemes
   -- { "ellisonleao/gruvbox.nvim" },
   -- { "savq/melange-nvim" },
-  { "navarasu/onedark.nvim" },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- { "navarasu/onedark.nvim" },
+  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
   -- Configure LazyVim load colorschem
   {
     "LazyVim/LazyVim",
     opts = {
+      -- colorscheme = "catppuccin-mocha",
+      colorscheme = "catppuccin-frappe",
       -- colorscheme = "catppuccin-macchiato",
-      colorscheme = "onedark",
+      -- colorscheme = "onedark",
       -- colorscheme = "gruvbox",
       -- colorscheme = "melange",
-      -- colorscheme = "catppuccin-mocha",
-      -- colorscheme = "catppuccin-frappe",
     },
   },
 }
