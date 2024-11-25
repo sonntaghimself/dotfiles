aeac6d6f4b6d327f2764863581e063f76da987d3
sonntaghimself <samuel@sonntag.tv>
2024-11-25 08:44:33 +0100

    Random Save

--- nix/flake.nix
+++ nix/flake.nix
@@ -34,6 +34,7 @@
             # "wezterm"
             # "kitty" # Nah for now, seems intersting though
             "alacritty"
+            # "r"
           ];
           brews = [
             "npm"
@@ -53,25 +54,25 @@
         ];
 
       # NOTE: Doesn't work.
-      system.activationScripts.applications.text = let
-        env = pkgs.buildEnv {
-          name = "system-applications";
-          paths = config.environment.systemPackages;
-          pathsToLink = "/Applications";
-        };
-      in
-        pkgs.lib.mkForce ''
-        # Set up applications.
-        echo "setting up /Applications..." >&2
-        rm -rf /Applications/Nix\ Apps
-        mkdir -p /Applications/Nix\ Apps
-        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-        while read src; do
-          app_name=$(basename "$src")
-          echo "copying $src" >&2
-          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-        done
-            '';
+      # system.activationScripts.applications.text = let
+      #   env = pkgs.buildEnv {
+      #     name = "system-applications";
+      #     paths = config.environment.systemPackages;
+      #     pathsToLink = "/Applications";
+      #   };
+      # in
+      #   pkgs.lib.mkForce ''
+      #   # Set up applications.
+      #   echo "setting up /Applications..." >&2
+      #   rm -rf /Applications/Nix\ Apps
+      #   mkdir -p /Applications/Nix\ Apps
+      #   find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+      #   while read src; do
+      #     app_name=$(basename "$src")
+      #     echo "copying $src" >&2
+      #     ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+      #   done
+      #       '';
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
